165238a435efa6e232cc90cabaa299f7e0f29242
Dmitriy <d.mace@live.com>
2019-11-21 17:51:40 +0300

    + add <summary>

--- DemoLib/UI/UserInterface.cs
+++ DemoLib/UI/UserInterface.cs
@@ -6,10 +6,19 @@ using System.Threading.Tasks;
 
 namespace DemoLib.UI
 {
+    /// <summary>
+    /// User Interface for the calculator
+    /// </summary>
     public class UserInterface : IUI
     {
+        /// <summary>
+        /// Output Message for the calculator
+        /// </summary>
         public Action<string, bool> Output => OutPutMessage;
 
+        /// <summary>
+        /// Input string for the calculator
+        /// </summary>
         public Func<string> Input => Console.ReadLine;
 
         private void OutPutMessage(string res, bool line)
