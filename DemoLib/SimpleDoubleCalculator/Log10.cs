f64f668db42c8cb0ff3d25775e27aded177857a0
Dmitriy <d.mace@live.com>
2019-11-21 17:00:19 +0300

    * modify exception, add <summary>

--- DemoLib/SimpleDoubleCalculator/Log10.cs
+++ DemoLib/SimpleDoubleCalculator/Log10.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLib.Verification;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +7,29 @@ using System.Threading.Tasks;
 
 namespace DemoLib.SimpleDoubleCalculator
 {
+    /// <summary>
+    /// Returns the Log10 of two double numbers
+    /// </summary>
     public class Log10 : BaseOperation, IOperation<double>
     {
+        /// <summary>
+        /// Initializes two double numbers
+        /// </summary>
+        /// <param name="x">X number</param>
+        /// <param name="y">Y number</param>
         public Log10(double x, double y = 0) : base(x, y) { }
+
+        /// <summary>
+        /// Calculates the Log10 of two double numbers
+        /// </summary>
+        /// <returns>Log10(X)</returns>
         public double Operate()
         {
             double result = Math.Log10(X);
+
             if (double.IsNaN(result))
-                throw new ArgumentException("Ошибка. Значение X меньше 0.");
+                Revise.ArgumentException(Resource.ValueOfXLessThanZero, nameof(X));                
+
             return result;
         }
     }
