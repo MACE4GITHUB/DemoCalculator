d434ec3878b5b5eb01a5b0ce7b8b734a5b0c6a39
Dmitriy <d.mace@live.com>
2019-11-21 18:01:08 +0300

    + add <summary>

--- DemoLib/IData.cs
+++ DemoLib/IData.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace DemoLib
 {
+    /// <summary>
+    /// Interface of data for the calculator
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IData<T>
     {
+        /// <summary>
+        /// Operation for the calculator
+        /// </summary>
         string Operation { get; }
+
+        /// <summary>
+        /// Value X for the calculator
+        /// </summary>
         T X { get; }
+
+        /// <summary>
+        /// Value Y for the calculator
+        /// </summary>
         T Y { get; }
     }
 }
--- DemoLib/IOperation.cs
+++ DemoLib/IOperation.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace DemoLib
 {
+    /// <summary>
+    /// Interface operation
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IOperation<T>
     {
+        /// <summary>
+        /// Operate operation
+        /// </summary>
+        /// <returns></returns>
         T Operate();
     }
 }
